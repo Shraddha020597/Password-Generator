05504bf7d44d24895259e3e6b7917f183d83e373
SPatel <118441579+Shraddha020597@users.noreply.github.com>
2025-05-08 14:05:28 +0000

    add port and gunicorn

--- main.py
+++ main.py
@@ -70,4 +70,4 @@ def read_log():
         return {"total_users": 0, "last_used": "", "users": [], "passwords": []}
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+  app.run(debug=False, host='0.0.0.0', port=5000)
